a4e92257f9cb89243ad92367c6aed0e7a890608e
Leonardo Andres Combita Castilla <leonardoandres89@gmail.com>
2023-09-22 22:41:23 -0500

    first commit

---
+++ Tinder_Jobs/client/src/Router.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Dev from './pages/Devs'
+
+
+
+// import AllDevs from './pages/AllDevs'
+// import LoginDev from './Components/LoginDevs'
+// import PerfilDev from './pages/perfilDev' 
+
+function MyRoutes () {
+    return (
+    <BrowserRouter>
+
+            <Routes>
+
+                <Route path = "/" element = {<Home/>}/>
+                <Route path = "/devs" element = {<Dev/>}/>
+
+
+
+
+                {/* <Route path = "/api/devs" element = {<AllDevs/>}/>
+                <Route path = "/api/devs/login" element = {<LoginDev/>}/> */}
+                {/* <Route path = "devs/perfil" element = {<PerfilDev/>}/> */}
+          
+            </Routes>
+    </BrowserRouter>
+    )
+}
+
+export default MyRoutes
---
+++ Tinder_Jobs/client/src/pages/PerfilDev.jsx
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+function PerfilDev () {
+
+    return (
+        <>
+        <h3>hola dev</h3>
+        </>
+    )
+
+    // const params = useParams()
+    // const [dev, setDev] = useState ([])
+
+    // useEffect (() =>{
+    //     axios.get('http://localhost:3001/api/devs/${params.id}')
+    // })
+}
+
+
+export default PerfilDev
